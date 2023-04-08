94a5245bf299f0b503e2420db3aa309f166b0d12
Carlos <dcl.carloslopes@gmail.com>
2023-04-08 13:10:01 -0300

    criando novas telas

--- tela_inicial.php
+++ tela_inicial.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/telainicial.css">
+    <link rel="stylesheet" href="css/tela_inicial.css">
     <title>Cantinho Gelado</title>
 </head>
 <body>
@@ -25,28 +25,30 @@
             <section class="card">
                 <img src="image/dinheiro.png" alt="">
                 <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Simili</span>
-                <button>Vendas</button>
+                <a href="vendas.php">
+                    <button>Vendas</button>
+                </a>
             </section>
             <section class="card">
                 <img src="image/caixa.png" alt="">
                 <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Simili</span>
-                <button>Cadastrar Vendas</button>
+                <a href="vendas.php">
+                    <button>Cadastrar vendas</button>
+                </a>
             </section>
             <section class="card">
                 <img src="image/avatar.png" alt="">
                 <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Simili</span>
-                <button>Criar novo Usuário</button>
+                <a href="vendas.php">
+                    <button>Criar novo usuário</button>
+                </a>
             </section>
         </div>
         <button class="btnSair">
             <a href='logout.php'>Sair</a>
         </button>
 
-
-
     </div>
 
-
-    
 </body>
 </html>
